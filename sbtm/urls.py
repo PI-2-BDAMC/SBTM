81daf85097127577ff18a9322d0327dbdb90353b
Omar Junior <omarfariajr@gmail.com>
2017-05-05 15:40:32 -0300

    Fix render bug

--- sbtm/urls.py
+++ sbtm/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import sbtmApp.views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('sbtmApp.urls',)),
+    url(r'^startEngine/', sbtmApp.views.startEngine, name="startEngine"),
+    url(r'^$', sbtmApp.views.index, name='index'),
 ]
--- sbtmApp/views.py
+++ sbtmApp/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, render
 
 def index(request):
     return render(request, 'index.html', {})
 
 def startEngine(request):
-	return render(request, 'startEngine.html', {})
-
+    return render(request, 'startEngine.html', {})
