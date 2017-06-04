45b046a7da21112c32413d02fd2481838b5f64e3
Omar Junior <omarfariajr@gmail.com>
2017-06-04 14:54:43 -0300

    Adding urls.py

--- sbtm/urls.py
+++ sbtm/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^startEngine/', sbtmApp.views.startEngine, name="startEngine"),
     url(r'^$', sbtmApp.views.index, name='index'),
+    url(r'^previousTests/', sbtmApp.views.previousTests, name="previousTests"),
+    url(r'^graphs/', sbtmApp.views.graphs, name="graphs"),
 ]
