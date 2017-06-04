6ca16ce693c85b1c5db57651e762322eda072bb4
Matheus Silva <matheussilva.fga@gmail.com>
2017-06-04 16:08:08 -0300

    Adding method to send control message to rasp
    Signed-off-by: ebenezerandrade <ebenezer.andrade1@gmail.com>

--- sbtmApp/views.py
+++ sbtmApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response, render
+import socket
 
 def index(request):
     return render(request, 'index.html', {})
@@ -10,4 +11,19 @@ def previousTests(request):
     return render(request, 'previousTests.html', {})
 
 def graphs(request):
-    return render(request, 'graphs.html', {})
+	sendMessage()
+	return render(request, 'graphs.html', {})
+
+def sendMessage():
+	TCP_IP = "192.168.0.21"
+	TCP_PORT = 5005
+	BUFFER_SIZE = 1024
+	MESSAGE = "Hello, World!"
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect((TCP_IP, TCP_PORT))
+	sock.send(MESSAGE)
+	data = sock.recv(BUFFER_SIZE)
+	sock.close()
+
+
