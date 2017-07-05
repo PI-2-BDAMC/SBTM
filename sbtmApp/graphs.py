5fa0fe6549d6ee4bb97f1f691e45436b7643e0a9
Matheus S. Pereira <matheussilva.fga@gmail.com>
2017-07-05 09:08:13 -0300

    Creating fuel comsuption and Admission Temperature graphs

--- sbtmApp/graphs.py
+++ sbtmApp/graphs.py
@@ -1,4 +1,5 @@
 from .models import BenchTest
+from datetime import datetime, timedelta
 
 class ChartData(object):
 
@@ -12,6 +13,17 @@ class ChartData(object):
         return data
 
     @classmethod
+    def get_admission_temperature(cls, user):
+        data = []
+        benchtests = BenchTest.objects.filter(userBenchTest = user)
+        sensor_0_temperature = benchtests.last().temperaturesensor_set.filter(localSensor=0).last().valueTemperature
+        sensor_1_temperature = benchtests.last().temperaturesensor_set.filter(localSensor=1).last().valueTemperature
+
+        data = [sensor_0_temperature, sensor_1_temperature]
+
+        return data
+
+    @classmethod
     def get_rpm_sensor_data(cls, user):
     	benchtests = BenchTest.objects.filter(userBenchTest = user)
     	rpm_data = benchtests.last().rpmsensor_set.last()
@@ -19,3 +31,20 @@ class ChartData(object):
         data = rpm_data.valueRPM
 
         return data
+
+    @classmethod
+    def get_fuel_comsumption(cls, user):
+    	benchtests = BenchTest.objects.filter(userBenchTest = user)
+    
+    	now = datetime.now()
+    	earlier = now - timedelta(seconds=30)
+
+    	cell_data_1 = 0
+
+    	cell_data_2 = benchtests.last().loadcell_set.last().valueCharge
+
+    	fuel_comsumption = (cell_data_1 - cell_data_2)/0.75
+
+    	data = fuel_comsumption
+
+    	return data
